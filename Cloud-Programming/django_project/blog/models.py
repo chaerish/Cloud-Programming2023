b18d8eda23b5b1b916b85c80058cc25600c35266
chaerish <ccyy8432@naver.com>
2023-03-28 17:06:07 +0900

    블로그 사진 파일 업로드

--- Cloud-Programming/django_project/blog/models.py
+++ Cloud-Programming/django_project/blog/models.py
@@ -8,7 +8,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-
+    head_image = models.ImageField(upload_to='blog/img/%Y/%m/%d', blank=True)
+    file_upload=models.FileField(upload_to="blog/img/%Y/%m/%d",blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- Cloud-Programming/django_project/blog/views.py
+++ Cloud-Programming/django_project/blog/views.py
@@ -1,3 +1,5 @@
+from django.views.generic import ListView, DetailView
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,19 +9,11 @@ from django.shortcuts import render
 from .models import Post
 
 
-def index(request):
-    #사용자의 요청을 받아서 이런일 저런일을...
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
 
-    return render(request, 'blog/index.html', {
-        'posts': posts,
-    })
+class PostDetail(DetailView):
+    model = Post
 
-
-def single_post_page(request, post_num): #글 하나씩 보여주는 거
-    post = Post.objects.get(pk=post_num)
-
-    return render(request, 'blog/single_post_page.html', {
-        'post': post,
-    })
