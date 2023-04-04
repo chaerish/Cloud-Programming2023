3010a72e75ffbcad7bae833b2295cc7c23f9bd3e
chaerish <ccyy8432@naver.com>
2023-04-04 15:30:06 +0900

    user 추가

--- Cloud-Programming/django_project/blog/models.py
+++ Cloud-Programming/django_project/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,19 +6,22 @@ import os.path
 from django.db import models
 
 
-
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    sub_title = models.CharField(max_length=30, null=True, default='')
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/img/%Y/%m/%d', blank=True)
-    file_upload=models.FileField(upload_to="blog/img/%Y/%m/%d",blank=True)
+    file_upload = models.FileField(upload_to="blog/img/%Y/%m/%d", blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # user // cascade= > 계단식으로 다 삭제해라
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE);
+
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
