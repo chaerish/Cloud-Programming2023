83b4171bfa878da789949ffbbda894164a10d59a
chaerish <ccyy8432@naver.com>
2023-04-04 10:17:34 +0900

    파일, 이미지 업로드

--- Cloud-Programming/django_project/blog/models.py
+++ Cloud-Programming/django_project/blog/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 # Create your models here.
-
+import os.path
 from django.db import models
+
 
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
     head_image = models.ImageField(upload_to='blog/img/%Y/%m/%d', blank=True)
     file_upload=models.FileField(upload_to="blog/img/%Y/%m/%d",blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -18,3 +21,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
